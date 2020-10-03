5b5a1b772c238b86a92ace3beba32e37145fa05d
Anna Kwon <kwonanna01@gmail.com>
2020-10-03 14:35:53 +0600

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    min = array[0];
+    for (let i = 0; i < array.length; i ++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+  }
+  else {
+    min = 0;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    max = array[0];
+    for (let i = 0; i < array.length; i ++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+  }
+  else {
+    max = 0;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let res = 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    for (let i = 0; i < array.length; i ++) {
+      sum += array[i];
+    }
+    res = sum / array.length;
+  }
+  return res;
 }
